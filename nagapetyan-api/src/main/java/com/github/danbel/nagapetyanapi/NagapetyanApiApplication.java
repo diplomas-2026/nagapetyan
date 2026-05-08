159e22be65f9b14acf44f6ed6f970698623e0709
DanBel <dan.bel.89@bk.ru>
2026-05-08 05:32:17 +0400

    init

---
+++ nagapetyan-api/src/main/java/com/github/danbel/nagapetyanapi/NagapetyanApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.danbel.nagapetyanapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NagapetyanApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NagapetyanApiApplication.class, args);
+    }
+
+}
---
+++ nagapetyan-api/src/test/java/com/github/danbel/nagapetyanapi/NagapetyanApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.danbel.nagapetyanapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NagapetyanApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
